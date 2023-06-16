335b3c3f892dbc5381f0924dd9b28fb993d9067e
Eliana Ayelén Granado <eligranado43@gmail.com>
2023-06-16 11:47:31 -0500

    first commit

---
+++ adivina-numero-secreto/adivinanza.js
@@ -0,0 +1,14 @@
+const generarNumeroAleatorio = () => {
+    return Math.floor(Math.random() * 100) + 1;;
+};
+
+
+const verificarAdivinanza = (numeroSecreto, numeroAdivinado) => {
+    if(numeroAdivinado < numeroSecreto){
+        console.log('El número secreto es mayor. ¡Sigue intentando!');
+    } else if (numeroAdivinado > numeroSecreto) {
+        console.log('El número secreto es menor. ¡Sigue intentando!');
+    } 
+}
+
+module.exports = { generarNumeroAleatorio, verificarAdivinanza }
